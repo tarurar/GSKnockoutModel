b12e6742ad0871e84cd94d2b2f0b332fee61080d
tarurar <tarutin_andrew@mail.ru>
2014-06-20 16:04:18 +0400

    backbone )

---
+++ TypeScriptGSModel/Scripts/_references.js
@@ -0,0 +1,2 @@
+﻿/// <reference path="jquery-2.0.0.js" />
+/// <autosync enabled="true" />
---
+++ TypeScriptGSModel/app.ts
@@ -0,0 +1,17 @@
+﻿/// <reference path="gs.BaseModel.ts"/>
+
+window.onload = () => {
+
+    var el = document.getElementById('content');
+
+    /*$.getJSON("/meeting.json", (data, status, jqXHR) => {
+
+        var model = ko.mapping.fromJS(data.d);
+        ko.applyBindings(model);
+    });*/
+
+    var meetingModel = new gsBaseModel.MeetingModel(gsBaseModel.GsMeetingItem, gsBaseModel.GsIssueItem, gsBaseModel.SPServerAPI.REST);
+    meetingModel.Load("1", new gsDataMgmnt.Callback((data) => {
+        ko.applyBindings(meetingModel);
+    }, () => {}));
+};
